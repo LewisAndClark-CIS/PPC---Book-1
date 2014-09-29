3ec3a2213a4dd397498fdeda03f8af96fe7c348f
MCoyne01 <MCoyne@StCharlesSD.org>
2014-09-29 09:54:16 -0500

    Challenges

---
+++ challenge1.py
@@ -0,0 +1,11 @@
+print('Hello World!')
+
+print('This is \na computer program \nthat prints on several lines')
+
+
+
+
+#Completed successfully: Yes
+#Errors: Yes, puttin newline character outside of quotes
+#Solved: Looking at examples online
+#Difficult: Nothing
---
+++ challenge2.py
@@ -0,0 +1,21 @@
+name = 'Bob'
+print('Hello ' + name)
+
+
+word1 = 'the '
+word2 = 'cat '
+word3 = 'sat '
+word4 = 'on '
+word5 = 'the '
+word6 = 'mat '
+
+print(word1+ word2+ word3+ word4+ word5+ word6)
+
+
+
+
+
+#Completed successfully: Yes
+#Erros: None
+#Solved: N/A
+#Difficult: None
