c33ec5f683d01d658b438f9b3194d30e903da6bf
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-01 03:18:29 -0500

    Challenge 1

---
+++ ChallengeNo1.py
@@ -0,0 +1,18 @@
+#################################################
+#Title: ChallengeNo1.py
+#Author: Sam Coon
+#Date: 9/29/14
+#################################################
+print("This is")
+print("a computer program")
+print("that prints on several lines")
+
+#################################################
+#Self review
+#Completed successfully?: Yes
+#Did you have any errors? What?: No errors
+#How did you solve them?: I didn't have any
+#What did you find difficult?: Nothing
+#################################################
+
+
