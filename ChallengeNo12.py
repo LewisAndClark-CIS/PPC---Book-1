0a7a6e299a35c1ff7b8878a7d5d1442d672f6375
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-01 03:22:34 -0500

    challenge 12

---
+++ ChallengeNo12.py
@@ -0,0 +1,22 @@
+######################################################
+#Title: ChallengeNo12.py
+#Author: Sam Coon
+#Date: 9/30/14
+######################################################
+
+import time
+
+question = input("What is your question? ")
+print("Let me think about that")
+time.sleep(10)
+input(question+ " ")
+print("lol")
+
+
+######################################################
+#Self review
+#Completed successfully?: Yes
+#Did you have any errors?: Yes
+#How did you solve them?: Trial and error, some research also
+#What did you find difficult?: figuring out how to use time.sleep()
+######################################################
