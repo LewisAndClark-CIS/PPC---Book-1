65a6b308aad8f62e9b4ebba9c0d0ce880f02f86e
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-01 03:20:20 -0500

    challenge 6

---
+++ ChallengeNo6.py
@@ -0,0 +1,20 @@
+######################################################
+#Title: ChallengeNo6.py
+#Author: Sam Coon
+#Date: 9/29/14
+######################################################
+
+DaysPerWeek = 7
+HoursPerDay = 24
+MinutesPerHour = 60
+print(MinutesPerHour*HoursPerDay*DaysPerWeek)
+
+######################################################
+#Self review
+#Completed successfully?: Yes
+#Did you have any errors? What?: No
+#How did you solve them?: I didn't have any
+#What did you find difficult?: Nothing
+
+        
+          
