a0a472a5c329cc65b975f73aed129543b03c921b
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-01 03:21:25 -0500

    challenge 10

---
+++ ChallengeNo10.py
@@ -0,0 +1,19 @@
+######################################################
+#Title: ChallengeNo10.py
+#Author: Sam Coon
+#Date: 9/30/14
+######################################################
+
+time = int(input("How many hours do you spend on a computer per day? "))
+if time < 2:
+    print("That seems reasonable")
+else:
+    print("Get a life")
+
+######################################################
+#Self review
+#Completed successfully?: Yes
+#Did you have any errors?: No
+#How did you solve them?: Had none
+#What did you find difficult?: Nothing
+######################################################
