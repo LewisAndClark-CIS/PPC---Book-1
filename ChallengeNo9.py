008808ebf9703758ef38b198015df3902faa112c
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-01 03:21:12 -0500

    challenge 9

---
+++ ChallengeNo9.py
@@ -0,0 +1,16 @@
+######################################################
+#Title: ChallengeNo9.py
+#Author: Sam Coon
+#Date: 9/30/14
+######################################################
+
+print("This is Sam" +"'" + "s first program")
+
+    
+######################################################
+#Self review
+#Completed successfully?: Yes
+#Did you have any errors?: No 
+#How did you solve them?: Had none
+#What did you find difficult?: Nothing
+######################################################
