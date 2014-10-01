a03b7caa0e2c0b09f7c5abb4644aad943eb6acef
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-01 03:22:46 -0500

    challenge 13

---
+++ ChallengeNo13.py
@@ -0,0 +1,37 @@
+######################################################
+#Title: ChallengeNo13.py
+#Author: Sam Coon
+#Date: 9/30/14
+######################################################
+import random
+import time
+
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+time.sleep(3)
+print(random.randint(0,100))
+
+######################################################
+#Self review
+#Completed successfully?: Yes
+#Did you have any errors?: No but I wanted to find a way to make this easier
+#How did you solve them?: I couldn't find one that I fully understood
+#What did you find difficult?: Trying to simplify this
+######################################################
