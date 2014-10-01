de382c2842ccfa9a8ddc244403897bd8e554c66c
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-01 03:19:49 -0500

    challenge 4

---
+++ ChallengeNo4.py
@@ -0,0 +1,18 @@
+#################################################
+#Title: ChallengeNo4.py
+#Author: Sam Coon
+#Date: 9/29/14
+#################################################
+
+a = 12
+b = 6
+c = (a+b)
+print(c)
+
+#################################################
+#Self review
+#Completed successfully?: Yes
+#Did you have any errors? What?: No
+#How did you solve the problem?: I didn't have any
+#What did you find difficult?: Nothing
+#################################################
