1fe6d1da0c572c539b4902a34566db4f5edea0e1
Sacoon@Stcharlessd.org <Sacoon@Stcharlessd.org>
2014-10-01 03:19:13 -0500

    challenge 2

---
+++ ChallengeNo2.py
@@ -0,0 +1,21 @@
+###############################################
+#Title: ChallengeNo2.py
+#Author: Sam Coon
+#Date: 9/29/14
+###############################################
+
+word1 = "the"
+word2 = "cat"
+word3 = "sat"
+word4 = "on"
+word5 = "the"
+word6 = "mat"
+print(word1 +" " + word2 + " " + word3 +" " + word4 + " " + word5 + " " + word6)
+
+###############################################
+#Self review
+#Completed successfully?: Yes.
+#Did you have any errors? What?: I tried to use commas for spaces.
+#How did you solve them?: I remebered that you could put space in quotation marks.
+#What did you find difficult?: Not really anything.
+###############################################
